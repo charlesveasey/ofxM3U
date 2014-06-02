1dffb528ac4253ad6ca37a2350b778fb8ebe955f
Charles Veasey <veasec@gmail.com>
2014-06-02 10:01:36 -0600

    Add source files

---
+++ src/ofxM3U.cpp
@@ -0,0 +1,91 @@
+#include "ofxM3U.h"
+
+#include "Poco/RegularExpression.h"
+using Poco::RegularExpression;
+
+void ofxM3U::load(string file){
+    ofBuffer buffer = ofBufferFromFile(file);
+    string header;
+    
+    if (buffer.size()) {
+        header = buffer.getNextLine();
+        if (header == "#EXTM3U"){
+            isExtended = true;
+            parseExtended(buffer);
+        }
+        else {
+            isExtended = false;
+            parseSimple(buffer);
+        }
+    }
+}
+
+void ofxM3U::parseExtended(ofBuffer buffer){
+    string header;
+    string line;
+    string file;
+    string title;
+    string length;
+    
+    while(buffer.isLastLine() == false) {
+        line = buffer.getNextLine();
+        
+        if (line.length()) {
+            header = line.substr(0, 8);
+            header = "#EXTINF:";
+            length = (ofSplitString(line, ",")[0]).substr(8);
+            title = line.substr(9+length.size());
+            title = trimString(title);
+            line = buffer.getNextLine();
+            file = line;
+            createExtendedItem(file, title, ofToInt(length));
+        }
+    }
+}
+
+void ofxM3U::parseSimple(ofBuffer buffer){
+    buffer.resetLineReader();
+    string line;
+    while(buffer.isLastLine() == false) {
+        line = buffer.getNextLine();
+        if (line.length())
+            createSimpleItem(line);
+        cout << line << endl;
+    }
+}
+
+void ofxM3U::createExtendedItem(string file, string title, int length){
+    M3UItem item;
+    item.file = file;
+    item.title = title;
+    item.length = length;
+    items.push_back(item);
+}
+
+void ofxM3U::createSimpleItem(string line){
+    M3UItem item;
+    item.file = line;
+    items.push_back(item);
+}
+
+vector<M3UItem> ofxM3U::getItems(){
+    return items;
+}
+
+void ofxM3U::clear(){
+    items.clear();
+}
+
+string ofxM3U::trimStringRight(string str) {
+    size_t endpos = str.find_last_not_of(" \t\r\n");
+    return (string::npos != endpos) ? str.substr( 0, endpos+1) : str;
+}
+
+string ofxM3U::trimStringLeft(string str) {
+    size_t startpos = str.find_first_not_of(" \t\r\n");
+    return (string::npos != startpos) ? str.substr(startpos) : str;
+}
+
+string ofxM3U::trimString(string str) {
+    return trimStringLeft(trimStringRight(str));
+}
---
+++ src/ofxM3U.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "ofMain.h"
+
+class M3UItem {
+    
+public:
+    string file;
+    string title;
+    int length;
+	
+};
+
+class ofxM3U {
+    
+public:
+	void load(string file);
+    void clear();
+    vector<M3UItem> getItems();
+    
+private:
+    vector<M3UItem> items;
+    ofBuffer buffer;
+    bool isExtended = false;
+    
+    void parseExtended(ofBuffer buffer);
+    void parseSimple(ofBuffer buffer);
+
+    void createExtendedItem(string file, string title, int length);
+    void createSimpleItem(string line);
+        
+    string trimStringRight(string str);
+    string trimStringLeft(string str);
+    string trimString(string str);
+};
+
